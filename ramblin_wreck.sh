ef04d3d38c8f1ff157349c451ff41f57c5a1ca91
Ross Lindsay <rossiv@gatech.edu>
2017-10-14 20:56:39 -0400

    Initial Commit

---
+++ ramblin_wreck.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+printf "\e[2J"
+printf "\033c"
+echo "I'm a Ramblin' Wreck from Georgia Tech"
+sleep 2.0
+echo "and a hell of an engineer—"
+sleep 1.5
+echo "A helluva,"
+sleep 0.4
+echo "helluva,"
+sleep 0.4
+echo "helluva,"
+sleep 0.4
+echo "helluva,"
+sleep 0.4
+echo "hell of an engineer."
+sleep 1.75
+echo "Like all the jolly good fellows,"
+sleep 1.5
+echo "I drink my whiskey clear."
+sleep 1.75
+echo "I'm a Ramblin' Wreck from Georgia Tech"
+sleep 1.75
+echo "and a hell of an engineer."
+sleep 1.75
+echo "Oh! If I had a daughter, sir,"
+sleep 1.5
+echo "I'd dress her in White and Gold,"
+sleep 1.5
+echo "And put her on the campus"
+sleep 1.7
+echo "to cheer the brave and bold."
+sleep 1.5
+echo "But if I had a son, sir, "
+sleep 1.75
+echo "I'll tell you what he'd do—"
+sleep 1.6
+echo "He would yell, 'To hell with georgia!'"
+sleep 1.5
+echo "like his daddy used to do."
+sleep 1.75
+echo "Oh, I wish I had a barrel of rum"
+sleep 1.5
+echo "and sugar three thousand pounds,"
+sleep 1.5
+echo "A college bell to put it in"
+sleep 1.75
+echo "and a clapper to stir it round."
+sleep 1.5
+echo "I'd drink to all the good fellows"
+sleep 1.5
+echo "who come from far and near."
+sleep 1.6
+echo "I'm a ramblin', gamblin',"
+sleep 2
+echo "hell of an engineer!"
+sleep 3.5
+echo "Go Jackets!"
+sleep 3
+echo "Go Jackets!"
+sleep 3.5
+echo "Go Jackets!"
+sleep 3
+echo "Go Jackets!"
+sleep 1.5
+echo "Fight, Drink, Win, Get Naked!"
+sleep 2
+echo ""
+echo ""
+cat gt_ascii.txt
+
